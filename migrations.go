8f335c10d55bad506d94f2e445f0c330546ca49d
Tyr Mactire <tyr@pettingzoo.co>
2022-05-29 21:19:44 -0700

    Fix Function Name (#2)

--- migrations.go
+++ migrations.go
@@ -65,7 +65,8 @@ func migrationFile() string {
 		if !ok {
 			break
 		}
-		if !strings.Contains(f.Function, "/migrate.") {
+		fmt.Printf("func: %s", f.Function)
+		if !strings.Contains(f.Function, "/immudb-migrate.") {
 			return f.File
 		}
 	}
